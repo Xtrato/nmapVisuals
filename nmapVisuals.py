4a9d5564a3b4b72df789f9c4b45a8acdc3938637
Xtrato <james.woolley5@googlemail.com>
2018-06-12 18:31:13 +0100

    initial commit

--- nmapVisuals.py
+++ nmapVisuals.py
@@ -1,6 +1,34 @@
 import csv
 import math
-from sty import fg, bg, ef, rs
+from flask import Flask, render_template, Markup
+from lxml import etree
+
+#Parses the scan log and orders the data into a list
+port = None
+address = None
+parsedServers = []
+#Iterates through the masscan XML file.
+for event, element in etree.iterparse('output.xml', tag="host"):
+    for child in element:
+        if child.tag == 'address':
+            #Assigns the current iterations address to the address variable.
+            exists = False
+            for entry in parsedServers:
+                if child.attrib['addr'] in entry:
+                    exists = True
+            parsedServers.append([child.attrib['addr'], 'port', 'port'])
+        if child.tag == 'ports':
+            for a in child:
+                print('yea')
+print(parsedServers)
+app = Flask(__name__)
+
+
+upperServiceRange = 0
+#get the port open per serice. If more than upperServiceRange then ports = upperServiceRange
+
+
+
 
 count = 1
 
@@ -10,19 +38,26 @@ with open('data.csv', 'r') as csvfile:
     your_list = list(reader)
 
 print(math.sqrt(len(your_list)))
-
+htmlBuffer = Markup('')
 
 for entry in your_list:
     if '1' in entry[1]:
       if count > int(math.sqrt(len(your_list))):
-        print(bg.red + ' ' + fg.rs)
         count = 0
+        htmlBuffer += Markup('<td>1 END</td></tr><tr>')
       else:
-        print(bg.red + ' ' + fg.rs,end=" ")
+          htmlBuffer += Markup('<td>1</td>')
     else:
       if count > int(math.sqrt(len(your_list))):
-        print(bg.blue + ' ' + fg.rs)
         count = 0
+        htmlBuffer += Markup('<td>0 END</td></tr><tr>')
       else:
-        print(bg.blue + ' ' + fg.rs,end=" ")
+          htmlBuffer += Markup('<td>0</td>')
     count = count + 1
+
+@app.route('/')
+def index():
+    return render_template('index.html', name=htmlBuffer)
+
+if __name__ == '__main__':
+    app.run()
