11f6ac96bd11d339e60834561ea77e0de981d5c2
james <james.woolley5@googlemail.com>
2018-07-11 22:08:16 +0100

    i diddnt mess up somehow..

--- nmapVisuals.py
+++ nmapVisuals.py
@@ -2,40 +2,25 @@ import csv
 import math
 from flask import Flask, render_template, Markup
 from lxml import etree
+import collections
 
-#Parses the scan log and orders the data into a list
+#Constructor
+
 port = None
 address = None
-parsedServers = {}
+parsedServers = collections.OrderedDict()
 
-#Iterates through the masscan XML file. Filters duplicates and stores IP's in dict with accompianing ports.
-for event, element in etree.iterparse('output.xml', tag="host"):
-    for child in element:
-        if child.tag == 'address':
-            ipAddress = child.attrib['addr']
-        if child.tag == 'ports':
-            for subChild in child:
-                port = [subChild.attrib['portid']]
-
-    if ipAddress in parsedServers:
-        portList = parsedServers[ipAddress]
-        portList.append(port)
-        parsedServers[ipAddress] = portList
-    else:
-        parsedServers[ipAddress] = [port]
-
-
-app = Flask(__name__)
-
-
-#Calculates the range of ports on devices. Used to produce the heatmap.
-upperServiceRange = 0
-for key, value in parsedServers.items():
-    if upperServiceRange < len(value):
-        upperServiceRange = len(value)
 
 
 #Finds the IP Range
+#Parses log file and populated ipList,
+ipList = []
+for event, element in etree.iterparse('output.xml', tag="host"):
+    for child in element:
+        if child.tag == 'address':
+            ipList.append(child.attrib['addr'])
+
+#iterates through ipList splits each IP into octets
 firstOctetRange = []
 secondOctetRange = []
 thirdOctetRange = []
@@ -43,13 +28,15 @@ forthOctetRange = []
 bitDelimeter = 0
 startingIP = 0
 endingIP = 0
-for key in parsedServers.items():
+for ip in ipList:
     binaryOctet = ''
-    octets = key[0].split('.')
+    octets = ip.split('.')
     firstOctetRange.append(int(octets[0]))
     secondOctetRange.append(int(octets[1]))
     thirdOctetRange.append(int(octets[2]))
     forthOctetRange.append(int(octets[3]))
+
+#calculates the octet where the IP address changes and saves value in bitDelimeter
 if min(firstOctetRange) != max(firstOctetRange):
     bitDelimeter = 0
 elif min(secondOctetRange) != max(secondOctetRange):
@@ -59,55 +46,77 @@ elif min(thirdOctetRange) != max(thirdOctetRange):
 elif min(forthOctetRange) != max(forthOctetRange):
     bitDelimeter = 3
 
+
+#Creates a ordered Dict which contains all IP addresses in the range taken from the log file.
 if bitDelimeter == 0:
     for one in range(min(firstOctetRange), max(firstOctetRange)):
         for two in range(0, 256):
             for three in range(0, 256):
                 for four in range(0, 256):
                     ip = "%d.%d.%d.%d" % (one, two, three, four)
-                    print(ip)
+                    parsedServers[ip] = []
 if bitDelimeter == 1:
     one = min(firstOctetRange)
     for two in range(min(secondOctetRange), max(secondOctetRange)):
         for three in range(0, 256):
             for four in range(0, 256):
                 ip = "%d.%d.%d.%d" % (one, two, three, four)
-                print(ip)
+                parsedServers[ip] = []
 if bitDelimeter == 2:
     one = min(firstOctetRange)
     two = min(secondOctetRange)
     for three in range(min(thirdOctetRange), max(thirdOctetRange)):
         for four in range(0, 256):
             ip = "%d.%d.%d.%d" % (one, two, three, four)
-            print(ip)
+            parsedServers[ip] = []
 if bitDelimeter == 3:
     one = min(firstOctetRange)
     two = min(secondOctetRange)
     three = min(thirdOctetRange)
-    for four in range(min(forthOctetRange), max(forthOctetRange)):
+    for four in range(min(forthOctetRange), max(forthOctetRange) + 1):
             ip = "%d.%d.%d.%d" % (one, two, three, four)
-            print(ip)
+            parsedServers[ip] = []
 
-print(firstOctetRange)
-print(secondOctetRange)
-print(thirdOctetRange)
-print(forthOctetRange)
+
+
+
+#Iterates through the masscan XML file. Filters duplicates and adds the ports to parsedServers orderedDict.
+
+for event, element in etree.iterparse('output.xml', tag="host"):
+    for child in element:
+        if child.tag == 'address':
+            ipAddress = child.attrib['addr']
+        if child.tag == 'ports':
+            for subChild in child:
+                port = [subChild.attrib['portid']]
+        parsedServers[ipAddress].append(port)
+
+
+
+app = Flask(__name__)
+
+#Calculates the range of ports on devices. Used to produce the heatmap.
+upperServiceRange = 0
+for key, value in parsedServers.items():
+    if upperServiceRange < len(value):
+        upperServiceRange = len(value)
+
 
 count = 0
-
 print(math.sqrt(len(parsedServers)))
 htmlBuffer = Markup('')
 
+
+
 for key, value in parsedServers.items():
     if len(value) < 2:
-        htmlBuffer += Markup('<td bgcolor="9fd1ff">' + str(key) + '</td>')
+        htmlBuffer += Markup('<td height="10", width="10", class="tooltip", bgcolor="9fd1ff"><span class="tooltiptext">' + str(key) + '</span></td>')
     if len(value) == 2:
-        htmlBuffer += Markup('<td bgcolor="fff99f">' + str(key) + '</td>')
+        htmlBuffer += Markup('<td height="10", width="10", class="tooltip", bgcolor="fff99f"><span class="tooltiptext">' + str(key) + '</span></td>')
     if len(value) > 2:
-        htmlBuffer += Markup('<td bgcolor="ff9f9f">' + str(key) + '</td>')
+        htmlBuffer += Markup('<td height="10", width="10", class="tooltip", bgcolor="ff9f9f"><span class="tooltiptext">' + str(key) + '</span></td>')
     count += 1
-    #if count > int(math.sqrt(len(parsedServers))):
-    if count > 6:
+    if count > math.sqrt(len(parsedServers)):
         htmlBuffer += Markup('</tr><tr>')
         count = 0
 
